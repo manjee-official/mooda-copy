6eca9c92f06b0dec3716d1ca0981717230933465
unman <manjee.official@gmail.com>
2020-09-12 17:45:20 +0900

    패키지 생성

--- MoodaCopy/app/src/main/java/com/manjee/mooda_copy/ui/MainActivity.kt
+++ MoodaCopy/app/src/main/java/com/manjee/mooda_copy/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.manjee.mooda_copy
+package com.manjee.mooda_copy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.manjee.mooda_copy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
